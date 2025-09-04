a17f4c02b264355ed3d29a4c769e4c00a050d054
bouazizisalim <salim.bouazizi@esprit.tn>
2025-09-04 17:37:18 +0100

    testt

--- src/main/java/com/esprit/formation/FormationApplication.java
+++ src/main/java/com/esprit/formation/FormationApplication.java
@@ -3,6 +3,7 @@ package com.esprit.formation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +15,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableAspectJAutoProxy
 @EnableAsync
 @EnableRetry
+@EnableJpaAuditing
+
 public class FormationApplication {
 
 	public static void main(String[] args) {

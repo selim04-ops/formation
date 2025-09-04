73242e8f60eceeeceb1a4e36863b1fb13aaf6007
bouazizisalim <salim.bouazizi@esprit.tn>
2025-09-04 17:17:08 +0100

    test

---
+++ src/test/java/com/esprit/formation/FormationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.esprit.formation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FormationApplicationTests {
+    @Test
+    void contextLoads() {
+        // Tests if the Spring context loads successfully
+    }
+}

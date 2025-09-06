9ac0cc547b01bdd7c915bbe16988930db76f7461
bouazizisalim <salim.bouazizi@esprit.tn>
2025-09-06 14:57:54 +0100

    testttttyytt

--- src/test/java/com/esprit/formation/FormationApplicationTest.java
+++ src/test/java/com/esprit/formation/FormationApplicationTest.java
@@ -2,8 +2,11 @@ package com.esprit.formation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 public class FormationApplicationTest {
     @Test
     void contextLoads() {
